a6e9641dcdb29372bcbe39905d87eb264bad8c53
sumedha <sumedhanarayanan@gmail.com>
2019-02-12 21:49:39 +0000

    dynamically adds chat messages to the board

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
+
+function randomId() {
+  return Math.random().toString(36).substr(2,6);
+}
+
+
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -29,15 +35,41 @@ class App extends Component {
         }
       ]
     }
+    this.addMessage = this.addMessage.bind(this);
   }
+
+  componentDidMount() {
+    console.log('componentDidMount <App />');
+    setTimeout(() => {
+      console.log('Simulating incoming message');
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
+  addMessage (content, username) {
+    const newMessage = {
+      id: randomId(),
+      username,
+      content
+    }
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({messages});
+  }
+
   render() {
     return (
       <div>
         <Navbar />
         <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser}/>
+        <ChatBar currentUser={this.state.currentUser} addMessage={this.addMessage}/>
       </div> 
     )
   }
 }
 export default App;
+
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,10 +3,24 @@ import React, {Component} from 'react';
 
 class ChatBar extends Component {
   render() {
+    const handleKeyPress = (event) => {
+      if(event.key === 'Enter' && event.target.name === "content"){
+        const messageContent = event.currentTarget.elements.content.value;
+        const username = event.currentTarget.elements.username.value;
+        this.props.addMessage(messageContent, username);
+        event.target.value='';
+      }
+    }
     return (
-      <footer className="chatbar">
-        <input className="chatbar-username" placeholder={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      <footer>
+      <form className="chatbar" onKeyPress={handleKeyPress}>
+        <input className="chatbar-username" placeholder={this.props.currentUser.name} name="username"/>
+        <input 
+          className="chatbar-message" 
+          placeholder="Type a message and hit ENTER" 
+          name="content"
+          />
+      </form>
       </footer>
     );
   }
